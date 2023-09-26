b435c68e061679a4dd1409db2c56b46b7e3d22c4
chandan kumar <chandankumar682003@gmail.com>
2023-09-26 16:20:55 +0530

    final commit

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var btn = document.querySelector("button");
+var outputArea = document.querySelector('.outputArea');
 
 
 btn.addEventListener("click", function () {
-    const text=document.querySelector('#inputText').value;
+    const text = document.querySelector('#inputText').value;
+    if (text !== "") {
+        //genrate url 
+        const url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + text;
 
-    //genrate url 
-    const url="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+text;
-    
+        // display output
+        document.querySelector('img').setAttribute('src', url);
+        //add border over qr
+        outputArea.style.border = "2px solid red";
+    }
+    else outputArea.textContent="Please Type Something";
 
-    // display output
-document.querySelector('img').setAttribute('src',url);
 })
